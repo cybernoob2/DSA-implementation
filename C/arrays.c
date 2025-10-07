2bee2592f504a2dac82c2bc994340963790c00ef
cybernoob2 <victorndira4@gmail.com>
2025-10-07 08:51:31 +0300

    First implementation: Arrays

---
+++ C/arrays.c
@@ -0,0 +1,63 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+           //ARRAYS
+           
+    // GETTING SUM OF ALL ELEMENTS
+    // int marks[] = {1,2,3,4,5,6,7,8,9};
+    // int results = 0; //Remember to initialize results
+    
+    // for(int i=0; i < 9; i++){
+    //     results += marks[i];
+    // }
+    // printf("%d", results);
+
+    
+    
+    // PRINTING ELEMENTS IN AN ARRAY
+    // int marks[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    // int length = sizeof(marks) / sizeof(marks[0]); //getting no. of elements
+    
+    // for(int i = 0; i < length; i++){
+    //     printf("%d, ", marks[i]);
+    // }
+    
+    
+    
+    // FINDING MINIMUM AND MAXIMUM IN AN ARRAY
+    // int arr[] = {21, 34, 64, 68, 32, 49, 98, 53, 88};
+    // int length = sizeof(arr) / sizeof(arr[0]);
+    // int max = arr[0], min = arr[0];
+    
+    // for(int i = 0; i < length; i++){
+    //     if(arr[i] > max){
+    //         max = arr[i];
+    //     }
+    //     if(arr[i] < min){
+    //         min = arr[i];
+    //     }
+    // }
+    // printf("Max = %d", max);
+    // printf("\nMin = %d", min);
+    
+    
+    
+    //REVERSE AN ARRAY
+    // int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    // int length = sizeof(arr) / sizeof(arr[0]);
+    
+    // for(int i = length - 1; i >= 0; i--){
+    //     printf("%d, ", arr[i]);
+    // }
+    
+    //COUNT EVEN AND ODD NUMBERS IN AN ARRAY
+    //SEARCH FOR AN ELEMENT
+    //COPY ELEMENTS FROM ONE ARRAY TO ANOTHER
+    //FIND DUPLICATE ELEMENTS
+    //SORT ELEMENTS IN ASCENDING AND DESCENDING ORDER
+    //MERGE TWO ARRAYS
+    
+
+    return 0;
+}
---
+++ Java/Arrays.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Arrays{
+    public static void main(String[]args){
+                      // ARRAYS
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        //ADDING ELEMENTS TO AN ARRAY
+        // ArrayList<Integer> marks = new ArrayList<Integer>();
+
+        // System.out.println("Enter number of students you would like to input their marks:");
+        // int mark = scanner.nextInt();
+
+        // for(int i = 0; i < mark; i++){
+        //     System.out.println("Enter marks:");
+        //     marks.add (scanner.nextInt());
+        // }
+        // System.out.println(marks);
+        
+        //REVERSE AN ARRAY
+        //COUNT EVEN AND ODD NUMBERS IN AN ARRAY
+        //SEARCH FOR AN ELEMENT
+        //COPY ELEMENTS FROM ONE ARRAY TO ANOTHER
+        //FIND DUPLICATE ELEMENTS
+        //SORT ELEMENTS IN ASCENDING AND DESCENDING ORDER
+        //MERGE TWO ARRAYS
+
+    }
+}
