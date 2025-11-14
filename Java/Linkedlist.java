1a8cb08c0ec47cdb155808c84f33c95f5d95693d
Victor Ndira <victorndira4@gmail.com>
2025-11-14 09:10:38 +0300

    Start of Linked lists

---
+++ Java/Linkedlist.java
@@ -0,0 +1,21 @@
+class Linkedlist{
+    public static void main(String[] args){
+	//Implement a singly linked list from scratch
+	//Insert a node at a given position
+	//Delete a node by value
+	//Reverse a linked list (iterative)
+	//Reverse a linked list (recursive)
+	//Find the middle element of a linked list
+	//Check if a linked list is a palindrome
+	//Remove duplicates from a sorted linked list
+	//Remove duplicates from an unsorted linked list
+	//Merge two sorted linked lists into one
+	//Detect a cycle in a linked list (Floyd’s algorithm)
+	//Remove a cycle from a linked list
+	//Find the Nth node from the end
+	//Sort a linked list using merge sort
+	//Implement a doubly linked list
+
+
+    }
+}
